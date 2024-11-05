2f453bfcd4d412e38107d70eb7b8cef3e00c1c86
leno1moraes <lenomoraes12@gmail.com>
2024-11-05 09:04:25 -0400

    structure auth

--- evenhi/src/app/header/header.component.ts
+++ evenhi/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoginComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
